3aca9a1e8c8df0eae2f37709c2bfb666ba7a2fed
HloWdMaster <1457391646@qq.com>
2018-12-17 15:34:59 +0800

    单链表实现

--- src/linkedlist/SLinkedList.java
+++ src/linkedlist/SLinkedList.java
@@ -1,36 +1,37 @@
 package linkedlist;
 
 /**
- * Create by 163 on 2018/12/14
+ * 单链表实现
  */
 public class SLinkedList<T> {
 
     private Node<T> head;   //头结点
-    private int size = 0;
+    private int size = 0;   //链表长度
+    private int modCount;   //
 
     public SLinkedList() {
-        head = new Node<>();
+        head = new Node<>(null);
     }
 
-    public void add(T data) {   //添加一个元素
-        head.data = data;
-        head.next = new Node<>();
+    public void insert(T data) {   //链表尾部添加一个元素
+        Node<T> p = new Node<>(data);
+        head.next = p;
         size++;
     }
 
-    public Node<T> remove(T data){  //删除一个元素
 
+    public Node<T> remove(T data){  //删除一个元素
+        // TODO: 2018/12/17
+        return new Node<T>(data);
     }
 
     private class Node<T> {
         T data;
         Node<T> next;
 
-        public Node() {
-        }
-
         public Node(T data) {
             this.data = data;
+            next = null;
         }
     }
 
